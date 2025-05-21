62840dbdff6883a4d95a5d1b62abb1058c806132
Luis Prado <e0957226681@live.uleam.edu.ec>
2025-05-21 00:11:09 -0500

    BIG ERROR

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,14 @@ Route::put('/paralelos/{id}', [ParaleloController::class, 'update']);
 Route::delete('/paralelos/{id}', [ParaleloController::class, 'destroy']);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 Route::get('/soap', [App\Http\Controllers\SoapController::class, 'consumirServicio']);
 =======
 >>>>>>> parent of ce6bea6 (AAA)
+=======
+>>>>>>> parent of 32c601b (update)
 =======
 >>>>>>> parent of 32c601b (update)
 =======
